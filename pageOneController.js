ed030ea05856b944f09fc4d5c59e064f1bcadcfd
Antonio Marcos S S Jr <amarcosju@gmail.com>
2017-09-04 16:34:10 -0300

    criado a parte de projetos

--- pageOneController.js
+++ pageOneController.js
@@ -1,4 +1,7 @@
- angular.module('myApp').controller('pageOne', function ($scope, $q, $timeout)  {
+ angular.module('myApp')
+ .controller('IPointController',IPointController)
+ .controller('pageOne', function ($scope, $q, $timeout,$mdDialog)  {
+
     
 var vm= this;
     vm.projects = [{
@@ -7,19 +10,59 @@ var vm= this;
         total: 15
         
     },{
-        name: 'projeto 3',
+        name: 'projeto 2',
         invested : 10,
         total: 15
     },
     {
-        name: 'projeto 2',
+        name: 'projeto 3',
         invested : 5,
         total: 15
     }
 ]
 
-    this.selectProject = function(){
-        alert('ahoy!')
+    this.selectProject = function(proj){
+         $mdDialog.show({
+          template:
+            '<md-dialog>' +
+            '  <md-dialog-content>Hello {{ employee }}!</md-dialog-content>' +
+            ' <input type="number" placeholder="Quantidade de pontos para investir" ng-model="pending" />'+
+            '  <md-dialog-actions>' +
+            '    <md-button ng-click="AddPoints()" class="md-primary">' +            
+            '      Adicionar Pontos' +
+            '    </md-button>' +
+            '    <md-button ng-click="closeDialog()" class="md-primary">' +
+            '      Fechar' +
+            '    </md-button>' +
+            '  </md-dialog-actions>' +
+            '</md-dialog>',
+          controller: 'IPointController',
+          onComplete: afterShowAnimation,
+           locals: { proj: proj }
+        });
+
+        // When the 'enter' animation finishes...
+
+        function afterShowAnimation(scope, element, options) {
+           // post-show code here: DOM element focus, etc.
+        }
 
     }
 })
+
+function IPointController($scope, $mdDialog, proj) {
+    // Assigned from construction <code>locals</code> options...
+    $scope.proj = proj;
+
+    $scope.AddPoints = function(){
+        alert($scope.pending);
+        proj.invested += $scope.pending
+        proj.total += $scope.pending
+      $mdDialog.hide();
+    }
+    $scope.closeDialog = function() {
+      // Easily hides most recent dialog shown...
+      // no specific instance reference is needed.
+      $mdDialog.hide(proj);
+    };
+  }
